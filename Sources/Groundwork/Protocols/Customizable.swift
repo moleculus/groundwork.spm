384843b102a2e46d31fbbe54f2deae4b0e108d39
Zakharov Viatcheslav <vz@moleculus.ru>
2021-02-04 18:33:24 +0100

    Fixes

--- Sources/Groundwork/Protocols/Customizable.swift
+++ Sources/Groundwork/Protocols/Customizable.swift
@@ -2,27 +2,11 @@ import UIKit
 
 public protocol Customizable {
     associatedtype T
-    func then(preservesSuperviewLayoutMargins: Bool, _ completion: (T) -> Void) -> T
+    func then(_ completion: (T) -> Void) -> T
 }
 
 extension Customizable {
-    public func then(preservesSuperviewLayoutMargins: Bool = true, _ completion: (Self) -> Void) -> Self {
-        if let view = self as? UIView {
-            view.preservesSuperviewLayoutMargins = true
-        }
-        
-        if let view = self as? UIScrollView {
-            view.preservesSuperviewLayoutMargins = true
-        }
-        
-        if let view = self as? UITableView {
-            view.preservesSuperviewLayoutMargins = true
-        }
-        
-        if let view = self as? UIScrollView {
-            view.preservesSuperviewLayoutMargins = true
-        }
-        
+    public func then(_ completion: (Self) -> Void) -> Self {
         completion(self)
         return self
     }
