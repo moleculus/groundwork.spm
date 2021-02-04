5dac05561fbebc9495df048a4ef80b8fd9f2b30d
Zakharov Viatcheslav <vz@moleculus.ru>
2021-02-04 09:55:10 +0100

    Colors initialization

--- Sources/Groundwork/Models/Colors.swift
+++ Sources/Groundwork/Models/Colors.swift
@@ -3,24 +3,49 @@ import UIKit
 public struct BackgroundColor: ColorPalette {
     public var light: UIColor
     public var dark: UIColor
+    
+    public init(light: UIColor, dark: UIColor? = nil) {
+        self.light = light
+        self.dark = dark ?? light
+    }
 }
 
 public struct TintColor: ColorPalette {
     public var light: UIColor
     public var dark: UIColor
+    
+    public init(light: UIColor, dark: UIColor? = nil) {
+        self.light = light
+        self.dark = dark ?? light
+    }
 }
 
 public struct TextColor: ColorPalette {
     public var light: UIColor
     public var dark: UIColor
+    
+    public init(light: UIColor, dark: UIColor? = nil) {
+        self.light = light
+        self.dark = dark ?? light
+    }
 }
 
 public struct SystemColor: ColorPalette {
     public var light: UIColor
     public var dark: UIColor
+    
+    public init(light: UIColor, dark: UIColor? = nil) {
+        self.light = light
+        self.dark = dark ?? light
+    }
 }
 
 public struct BorderColor: ColorPalette {
     public var light: UIColor
     public var dark: UIColor
+    
+    public init(light: UIColor, dark: UIColor? = nil) {
+        self.light = light
+        self.dark = dark ?? light
+    }
 }
