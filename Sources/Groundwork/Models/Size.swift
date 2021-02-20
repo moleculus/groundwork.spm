1714ec8e8c39cd0f5681534bab862e21db41a0c3
Zakharov Viatcheslav <vz@moleculus.ru>
2021-02-20 18:53:54 +0100

    fixes

--- Sources/Groundwork/Models/Size.swift
+++ Sources/Groundwork/Models/Size.swift
@@ -3,4 +3,9 @@ import UIKit
 public struct Size {
     public let width: Value
     public let height: Value
+    
+    public init(width: Value, height: Value) {
+        self.width = width
+        self.height = height
+    }
 }
