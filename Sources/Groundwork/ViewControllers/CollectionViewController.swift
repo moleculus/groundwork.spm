ba36c1119acc605e34aff5e9206303c0a600ec58
Zakharov Viatcheslav <vz@moleculus.ru>
2021-02-20 20:27:09 +0100

    asdfs

--- Sources/Groundwork/ViewControllers/CollectionViewController.swift
+++ Sources/Groundwork/ViewControllers/CollectionViewController.swift
@@ -38,4 +38,12 @@ open class CollectionViewController<Section: Groundwork.Section>: ViewController
         return sections[indexPath.section].sizeForItemAt(at: indexPath, in: collectionView)
     }
     
+    open func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    open func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
 }
