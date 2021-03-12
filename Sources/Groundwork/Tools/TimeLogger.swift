6cffe746d334a8a7245050f3e1ab90a5cba3b857
Zakharov Viatcheslav <vz@moleculus.ru>
2021-03-12 19:14:57 +0100

    Adds 'TimeLogger'

---
+++ Sources/Groundwork/Tools/TimeLogger.swift
@@ -0,0 +1,25 @@
+public class TimeLogger {
+    
+    // MARK: - Properties.
+    
+    private var stamps: [(date: Date, message: String)] = [] {
+        didSet {
+            guard stamps.count > 1 else {
+                return
+            }
+            
+            let count = stamps.count
+            let start = stamps[count - 2]
+            let end = stamps[count - 1]
+            
+            print(end.date.timeIntervalSince1970 - start.date.timeIntervalSince1970, end.message)
+        }
+    }
+    
+    // MARK: - Public Methods.
+    
+    public func logTime(message: String = "") {
+        stamps.append((date: Date(), message: message))
+    }
+    
+}
