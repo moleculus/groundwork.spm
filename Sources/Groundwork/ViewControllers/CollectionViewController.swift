df4277f3a29ce92a00efb4805f9f1e2d463311a6
Zakharov Viatcheslav <vz@moleculus.ru>
2021-03-18 09:56:55 +0100

    Fixes

--- Sources/Groundwork/ViewControllers/CollectionViewController.swift
+++ Sources/Groundwork/ViewControllers/CollectionViewController.swift
@@ -9,13 +9,19 @@ open class CollectionViewController<View: CollectionBasedView, Section: Groundwo
         
     // MARK: - Computed Properties.
     
-    open var collectionViewLayout: UICollectionViewLayout {
+    private var collectionViewLayout: UICollectionViewLayout {
         return UICollectionViewCompositionalLayout(sectionProvider: { (section, _) -> NSCollectionLayoutSection? in
             let section = self.sections[section]
-            return section.layoutSection(for: self.ui.collectionView)
+            let layoutSection = section.layoutSection(for: self.ui.collectionView)
+            
+            layoutSection.visibleItemsInvalidationHandler = { [weak self] (visibleItems, contentOffset, environment) in
+                self?.handleLayoutChanges(in: section, visibleItems: visibleItems, contentOffset: contentOffset, environment: environment)
+            }
+            
+            return layoutSection
         })
     }
-    
+        
     // MARK: - Initialization.
     
     public init() {
@@ -31,6 +37,16 @@ open class CollectionViewController<View: CollectionBasedView, Section: Groundwo
         self.biggestTopSafeAreaInset = max(ui.safeAreaInsets.top, biggestTopSafeAreaInset)
     }
     
+    // MARK: - Public Methods.
+    
+    open func handleLayoutChanges(in section: Section, visibleItems: [NSCollectionLayoutVisibleItem], contentOffset: CGPoint, environment: NSCollectionLayoutEnvironment) {
+        
+    }
+    
+    open func scrollToTop(animated: Bool) {
+        ui.collectionView.setContentOffset(CGPoint(x: 0, y: -biggestTopSafeAreaInset), animated: animated)
+    }
+    
     // MARK: - UICollectionViewDataSource.
     
     open func numberOfSections(in collectionView: UICollectionView) -> Int {
@@ -43,12 +59,6 @@ open class CollectionViewController<View: CollectionBasedView, Section: Groundwo
     
     open func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         return sections[indexPath.section].cellForItemAt(at: indexPath, in: collectionView)
-    }
-    
-    // MARK: - ScrollsToTop.
-    
-    open func scrollToTop(animated: Bool) {
-        ui.collectionView.setContentOffset(CGPoint(x: 0, y: -biggestTopSafeAreaInset), animated: animated)
     }
     
 }
