8cbbcc814337c02c09f130ba63b7df26870e6867
Zakharov Viatcheslav <vz@moleculus.ru>
2021-03-31 13:45:48 +0200

    Update LayoutSection.swift

--- Sources/Groundwork/Models/LayoutSection.swift
+++ Sources/Groundwork/Models/LayoutSection.swift
@@ -1,10 +1,62 @@
 import UIKit
 
-public protocol LayoutSection: LayoutSectionDataSource, LayoutSectionDelegate {
-    func layoutSection(in collectionView: UICollectionView) -> NSCollectionLayoutSection
-}
-
-extension LayoutSection {
+public class LayoutSection {
+            
+    // MARK: - DataSource.
+    
+    public func numberOfItems() -> Int {
+        return 1
+    }
+    
+    public func cell(for collectionView: UICollectionView, at indexPath: IndexPath) -> UICollectionViewCell {
+        fatalError()
+    }
+    
+    public func decorationItems() -> [NSCollectionLayoutDecorationItem] {
+        return []
+    }
+    
+    // MARK: - Layout.
+    
+    public func scrollDirection() -> UICollectionView.ScrollDirection {
+        return .vertical
+    }
+    
+    public func itemSize(in collectionView: UICollectionView) -> NSCollectionLayoutSize {
+        fatalError()
+    }
+    
+    public func groupInterItemSpacing() -> NSCollectionLayoutSpacing? {
+        return nil
+    }
+    
+    public func groupEdgeSpacing() -> NSCollectionLayoutEdgeSpacing? {
+        return nil
+    }
+    
+    public func sectionInsets(in collectionView: UICollectionView) -> NSDirectionalEdgeInsets {
+        return .zero
+    }
+    
+    public func sectionInterGroupSpacing() -> CGFloat {
+        return 0
+    }
+    
+    public func layoutGroup(in collectionView: UICollectionView) -> NSCollectionLayoutGroup {
+        let itemSize = self.itemSize(in: collectionView)
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let group = NSCollectionLayoutGroup.vertical(
+            layoutSize: itemSize,
+            subitems: [item]
+        )
+        
+        group.interItemSpacing = groupInterItemSpacing()
+        group.edgeSpacing = groupEdgeSpacing()
+        
+        return group
+    }
+    
     public func layoutSection(in collectionView: UICollectionView) -> NSCollectionLayoutSection {
         let layoutSection = NSCollectionLayoutSection(group: layoutGroup(in: collectionView))
         layoutSection.contentInsets = sectionInsets(in: collectionView)
@@ -19,51 +71,9 @@ extension LayoutSection {
             preconditionFailure()
         }
         
+        layoutSection.interGroupSpacing = sectionInterGroupSpacing()
+        
         return layoutSection
     }
-}
-
-public protocol LayoutSectionDataSource {
-    func numberOfItems() -> Int
-    func cell(for collectionView: UICollectionView, at indexPath: IndexPath) -> UICollectionViewCell
-    func decorationItems() -> [NSCollectionLayoutDecorationItem]
-}
-
-extension LayoutSectionDataSource {
-    public func numberOfItems() -> Int {
-        return 1
-    }
         
-    public func decorationItems() -> [NSCollectionLayoutDecorationItem] {
-        return []
-    }
-}
-
-public protocol LayoutSectionDelegate {
-    func scrollDirection() -> UICollectionView.ScrollDirection
-    func itemSize(in collectionView: UICollectionView) -> NSCollectionLayoutSize
-    func sectionInsets(in collectionView: UICollectionView) -> NSDirectionalEdgeInsets
-    func layoutGroup(in collectionView: UICollectionView) -> NSCollectionLayoutGroup
-}
-
-extension LayoutSectionDelegate {
-    public func scrollDirection() -> UICollectionView.ScrollDirection {
-        return .vertical
-    }
-    
-    public func sectionInsets(in collectionView: UICollectionView) -> NSDirectionalEdgeInsets {
-        return .zero
-    }
-    
-    public func layoutGroup(in collectionView: UICollectionView) -> NSCollectionLayoutGroup {
-        let itemSize = self.itemSize(in: collectionView)
-        let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        
-        let group = NSCollectionLayoutGroup.vertical(
-            layoutSize: itemSize,
-            subitems: [item]
-        )
-        
-        return group
-    }
 }
